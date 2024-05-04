be2cc2184a1f96a690e3e1a5dd1a24610a303a83
Yash-978 <virwalyash@gmail.com>
2024-05-04 09:54:01 +0530

    Initial Commit

--- lib/CARTSCREEN/cartScreen.dart
+++ lib/CARTSCREEN/cartScreen.dart
@@ -20,7 +20,8 @@ class _CartScreenState extends State<CartScreen> {
         centerTitle: true,
         title: Text('YOUR CART'),
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           SingleChildScrollView(
             child: Column(
@@ -117,116 +118,119 @@ class _CartScreenState extends State<CartScreen> {
     );
   }
 
-  Container udfCart(int index,
+  Widget udfCart(int index,
       {required String img,
       required String name,
       required int price,
       required int member}) {
-    return Container(
-      height: 200,
-      width: 400,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        border: Border.all(color: Colors.black),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-            height: 200,
-            width: 120,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  cartList[index]['img'],
-                ),
-              ),
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                cartList[index]['name'],
-                style: TextStyle(fontSize: 20),
-              ),
-              Text(
-                cartList[index]['price'].toString(),
-                style: TextStyle(fontSize: 20),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        if (cartList[index]['member'] > 1) {
-                          cartList[index]['member']--;
-                        }
-                      });
-                    },
-                    child: Container(
-                      height: 45,
-                      width: 45,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          border: Border.all(color: Colors.black)),
-                      child: Icon(Icons.remove),
-                    ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 200,
+        width: 400,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(color: Colors.black),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: 200,
+              width: 120,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    cartList[index]['img'],
                   ),
-                  Container(
-                    height: 45,
-                    width: 45,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all(color: Colors.black)),
-                    child: Center(
-                      child: Text(
-                        cartList[index]['member'].toString(),
-                        style: TextStyle(fontSize: 20),
+                ),
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  cartList[index]['name'],
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  cartList[index]['price'].toString(),
+                  style: TextStyle(fontSize: 20),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (cartList[index]['member'] > 1) {
+                            cartList[index]['member']--;
+                          }
+                        });
+                      },
+                      child: Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(color: Colors.black)),
+                        child: Icon(Icons.remove),
                       ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        cartList[index]['member']++;
-                      });
-                    },
-                    child: Container(
+                    Container(
                       height: 45,
                       width: 45,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(15),
                           border: Border.all(color: Colors.black)),
-                      child: Icon(Icons.add),
+                      child: Center(
+                        child: Text(
+                          cartList[index]['member'].toString(),
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                cartList.removeAt(index);
-                qty=0;
-                amount=0;
-                total=0;
-              });
-            },
-            child: Container(
-              height: 45,
-              width: 45,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(color: Colors.black)),
-              child: Icon(Icons.delete),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          cartList[index]['member']++;
+                        });
+                      },
+                      child: Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(color: Colors.black)),
+                        child: Icon(Icons.add),
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
-          ),
-        ],
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  cartList.removeAt(index);
+                  qty=0;
+                  amount=0;
+                  total=0;
+                });
+              },
+              child: Container(
+                height: 45,
+                width: 45,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Colors.black)),
+                child: Icon(Icons.delete),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
